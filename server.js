08e2422cf747f4e8f9aae4d1a7559f7751681581
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-22 00:29:58 +0300

    добавила хост из конфига

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const config = require('./config');
 const app = require('./app');
 
-app.listen(config.app.port, () => {
-  console.log(`Слушаю порт ${config.app.port}`);
+app.listen(config.app.port, config.app.host, () => {
+  console.log(`Слушаю порт ${config.app.port} на хосте ${config.app.host}`);
 });
 

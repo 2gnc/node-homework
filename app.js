e61b4986219acbee21dfd38d1f12c1c249461b60
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-21 11:57:34 +0300

    настроила роутинг

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
-const path = require('path');
 const express = require('express');
 const Gitloader = require('./gitloader');
+const index = require('./routes/index');
 
 const app = express();
 
 // определяем шаблонизатор
 app.set('view engine', 'pug');
 
-app.get('/', (req, res) => {
-  res.render('index', {
-    title: 'Проверка', 
-    message: 'Hello there!',
-  });
-});
+app.use('/', index);
 
 const gtl = new Gitloader();
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,17 @@
-const app = require('../app');
+const express = require('express');
 
-// здесь должны быть роуты
+const router = express.Router();
+
+router.use((req, res, next) => {
+  console.log('специфический обработчик именно для этого маршрута');
+  next();
+});
+
+router.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Проверка2',
+    message: 'Привет, я роутер',
+  });
+});
+
+module.exports = router;

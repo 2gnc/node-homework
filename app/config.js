e89128771b373602a166333e6d3b1d41b84f09fd
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-22 23:46:40 +0300

    добавила ссылку на форматы дат

--- app/config.js
+++ app/config.js
@@ -1,9 +1,12 @@
+// возможные форматы даты: https://momentjs.com/docs/#/parsing/
+
 const config = {
   app: {
     host: 'localhost',
     port: 3000,
   },
   repo: {
+    dateFormat: '',
     path: '/home/ksenia/learn/shri/05-nodejs/node-homework',
   },
 };

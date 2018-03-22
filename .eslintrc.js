2bc834e543785a076e5ce409a653c97fdf31dc86
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-22 12:59:30 +0300

    уточнила правила линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "padded-blocks": 1,
-        "no-trailing-spaces": 1
+        "no-trailing-spaces": 1,
+        "space-in-parens": ["error", "never"]
     }
 };

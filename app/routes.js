79d7ac5d9381dd1b49cc299a77c1c33136147a8a
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-22 14:26:36 +0300

    закоментировала ненужный код

--- app/routes.js
+++ app/routes.js
@@ -5,12 +5,12 @@ const brdsp = require('./controllers/display-branch');
 const router = express.Router();
 const gtl = new Gitloader();
 
-router.use((req, res, next) => {
-  // специфический обработчик именно для этого маршрута
-  /* ... */
-  // next(); передает управление дальше
-  next();
-});
+// router.use((req, res, next) => {
+//   // специфический обработчик именно для этого маршрута
+//   /* ... */
+//   // next(); передает управление дальше
+//   next();
+// });
 
 router.get('/', (req, res) => {
 

8ecdf1d8b05a23792d95c675f38f68b4b7dd428b
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-23 19:12:54 +0300

    исключила пустые коммиты из массива

--- app/controllers/get-commits.js
+++ app/controllers/get-commits.js
@@ -8,16 +8,18 @@
 
 const getCommits = (stdout) => {
 
-  const commits = stdout.split('++').map((item) => {
-    const arr = item.split(/\n/).filter(itm => itm.length > 0);
-    return {
-      commitHash: arr[0],
-      treeHash: arr[1],
-      comitter: arr[2],
-      timestamp: arr[3],
-      subject: arr[4],
-    };
-  });
+  const commits = stdout.split('++')
+    .filter(itm => itm.length > 0)
+    .map((item) => {
+      const arr = item.split(/\n/).filter(itm => itm.length > 0);
+      return {
+        commitHash: arr[0],
+        treeHash: arr[1],
+        comitter: arr[2],
+        timestamp: arr[3],
+        subject: arr[4],
+      };
+    });
   return commits;
 };
 module.exports = getCommits;

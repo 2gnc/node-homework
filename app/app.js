fcc7b2cc1ba72ca8e315ddd48e691161769a12c6
Ksenia Polyakova <polyakova.kse@yandex.ru>
2018-03-23 23:28:41 +0300

    объединила server и app

--- app/app.js
+++ app/app.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const express = require('express');
-const index = require('./routes');
+const routes = require('./routes');
+const config = require('./config');
 
 const app = express();
 
 // определяем шаблонизатор
 app.set('views', path.join('app/views'));
 app.set('view engine', 'pug');
-
+// статические файлы
 app.use(express.static(path.join('public')));
 
-app.use('/', index);
+app.use('/', routes);
+
+app.listen(config.app.port, config.app.host, () => {
+  console.log(`Слушаю порт ${config.app.port} на хосте ${config.app.host}`);
+}); 
 
 module.exports = app;
--- app/server.js
+++
@@ -1,7 +0,0 @@
-const config = require('./config');
-const app = require('./app');
-
-app.listen(config.app.port, config.app.host, () => {
-  console.log(`Слушаю порт ${config.app.port} на хосте ${config.app.host}`);
-});
-
